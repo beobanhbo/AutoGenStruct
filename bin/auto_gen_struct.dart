c096f47792016137377718275c2b3c230724fdb1
quoctran <tranquoc1171@gmail.com>
2023-03-13 09:37:43 +0700

    Update auto_gen_struct.dart

--- bin/auto_gen_struct.dart
+++ bin/auto_gen_struct.dart
@@ -1,3 +1,5 @@
 import 'package:auto_gen_struct/cmd/auto_gen_struct_cli.dart';
 
-void main(List<String> arguments) => AutoGenStruct().run(arguments);
+void main(List<String> arguments) {
+  print('Hello world: ${6 * 7}!');
+}
